e1d72047ae738d9736acc42fbcfb0b7eace58b7b
Vladimir Shapkarin <vshapkarin@mail.ru>
2019-04-10 18:06:18 +0300

    Adding update method. Upgrading methods: save, get, delete. Adding setter and getter to Resume.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,40 +4,65 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private static final int ARRAY_LENGTH = 10000;
+    private Resume[] storage = new Resume[ARRAY_LENGTH];
     private int size;
 
     void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
-        storage[size] = r;
-        size++;
+    private int contains(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void update(Resume resume) {
+        int index = this.contains(resume.getUuid());
+        if (index != -1) {
+            storage[index] = resume;
+        } else {
+            System.out.println("Resume doesn't exist!");
+        }
+    }
+
+    void save(Resume resume) {
+        if (size == ARRAY_LENGTH) {
+            System.out.println("Storage is already full!");
+            return;
+        }
+
+        if (this.contains(resume.getUuid()) == -1) {
+            storage[size] = resume;
+            size++;
+        } else {
+            System.out.println("Resume already exists!");
+        }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size; i++){
-            if (uuid.equals(storage[i].uuid)) {
-                return storage[i];
-            }
+        int index = this.contains(uuid);
+        if (index != -1) {
+            return storage[index];
+        } else {
+            System.out.println("Resume doesn't exist!");
+            return null;
         }
-        return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                for (; i < size - 1; i++) {
-                    storage[i] = storage[i + 1];
-                }
-                storage[storage.length - 1] = null;   //If the array was full
-                size--;
-                break;
-            }
+        int index = this.contains(uuid);
+        if (index != -1) {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        } else {
+            System.out.println("Resume doesn't exist!");
         }
     }
 
