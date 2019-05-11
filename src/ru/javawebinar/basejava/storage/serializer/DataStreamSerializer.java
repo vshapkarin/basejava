2eb7a99ed552f5334368d68d0c09f800b2e49b4c
Vladimir Shapkarin <vshapkarin@mail.ru>
2019-05-12 00:26:31 +0300

    HW10: new funct interface IOConsumer; new method forEachData in DSSerializer; refactored doWrite using lambdas.

--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -1,12 +1,12 @@
 package ru.javawebinar.basejava.storage.serializer;
 
 import ru.javawebinar.basejava.model.*;
+import ru.javawebinar.basejava.util.IOConsumer;
 
 import java.io.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.UnaryOperator;
 
 public class DataStreamSerializer implements SerializationStrategy {
 
@@ -34,11 +34,11 @@ public class DataStreamSerializer implements SerializationStrategy {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        writeList(entry, dos);
+                        writeList(((TextListSection) entry.getValue()).getContent(), dos);
                         break;
                     case EXPERIENCE:
                     case EDUCATION:
-                        writeTimePeriod(entry, dos);
+                        writeTimePeriod(((TimePeriodSection) entry.getValue()).getContent(), dos);
                         break;
                     default:
                         break;
@@ -61,18 +61,19 @@ public class DataStreamSerializer implements SerializationStrategy {
             size = dis.readInt();
             for (int i = 0; i < size; i++) {
                 String sectionName = dis.readUTF();
+                SectionType section = SectionType.valueOf(sectionName);
                 switch (sectionName) {
                     case "PERSONAL":
                     case "OBJECTIVE":
-                        resume.addSection(SectionType.valueOf(sectionName), new TextOnlySection(dis.readUTF()));
+                        resume.addSection(section, new TextOnlySection(dis.readUTF()));
                         break;
                     case "ACHIEVEMENT":
                     case "QUALIFICATIONS":
-                        resume.addSection(SectionType.valueOf(sectionName), new TextListSection(readList(dis)));
+                        resume.addSection(section, new TextListSection(readList(dis)));
                         break;
                     case "EXPERIENCE":
                     case "EDUCATION":
-                        resume.addSection(SectionType.valueOf(sectionName), new TimePeriodSection(readTimePeriod(dis)));
+                        resume.addSection(section, new TimePeriodSection(readTimePeriod(dis)));
                         break;
                     default:
                         break;
@@ -82,40 +83,28 @@ public class DataStreamSerializer implements SerializationStrategy {
         }
     }
 
-    private void writeList(Map.Entry<SectionType, AbstractSection> entry, DataOutputStream dos) throws IOException {
-        TextListSection section = (TextListSection) entry.getValue();
-        dos.writeInt(section.getContent().size());
-        for (String str : section.getContent()) {
-            dos.writeUTF(str);
-        }
+    private void writeList(List<String> listContent, DataOutputStream dos) throws IOException {
+        dos.writeInt(listContent.size());
+        forEachData(listContent, dos::writeUTF);
     }
 
-    private void writeTimePeriod(Map.Entry<SectionType, AbstractSection> entry, DataOutputStream dos) throws IOException {
-        List<TimePeriodOrganisation> organisations = ((TimePeriodSection) entry.getValue()).getContent();
-        dos.writeInt(organisations.size());
-        for (TimePeriodOrganisation organisation : organisations) {
-            dos.writeUTF(organisation.getHomePage().getName());
-            String url = organisation.getHomePage().getUrl();
-            if(url != null) {
-                dos.writeUTF(url);
-            } else {
-                dos.writeUTF("");
-            }
+    private void writeTimePeriod(List<TimePeriodOrganisation> organisations, DataOutputStream dos) throws IOException {
+        UnaryOperator<String> nullWriter = a -> a != null ? a : "";
 
-            List<TimePeriodOrganisation.TimePeriod> periods = organisation.getPeriods();
+        dos.writeInt(organisations.size());
+        forEachData(organisations, a -> {
+            dos.writeUTF(a.getHomePage().getName());
+            dos.writeUTF(nullWriter.apply(a.getHomePage().getUrl()));
+
+            List<TimePeriodOrganisation.TimePeriod> periods = a.getPeriods();
             dos.writeInt(periods.size());
-            for (TimePeriodOrganisation.TimePeriod period : periods) {
-                dos.writeUTF(period.getStart().toString());
-                dos.writeUTF(period.getEnd().toString());
-                dos.writeUTF(period.getText());
-                String optionalText = period.getOptionalText();
-                if(optionalText != null) {
-                    dos.writeUTF(optionalText);
-                } else {
-                    dos.writeUTF("");
-                }
-            }
-        }
+            forEachData(periods, b -> {
+                dos.writeUTF(b.getStart().toString());
+                dos.writeUTF(b.getEnd().toString());
+                dos.writeUTF(b.getText());
+                dos.writeUTF(nullWriter.apply(b.getOptionalText()));
+            });
+        });
     }
 
     private List<String> readList(DataInputStream dis) throws IOException {
@@ -149,6 +138,13 @@ public class DataStreamSerializer implements SerializationStrategy {
             organisations.add(new TimePeriodOrganisation(new Contact(name, url), periods));
         }
         return organisations;
+    }
+
+    private <E> void forEachData(Collection<? extends E> collection, IOConsumer<? super E> action) throws IOException {
+        Objects.requireNonNull(action);
+        for (E e : collection) {
+            action.accept(e);
+        }
     }
 }
 
